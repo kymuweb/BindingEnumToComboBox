9b6b9ba377390c7d4ccb85f3800ea40b4e346559
Javier Garcia <kymuweb@gmail.com>
2017-01-31 17:12:22 +0000

    BindingEnumToComboBox
    Just a simple example of how to use the description attribute of an enum type into a combo box.

---
+++ EnumClass.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace EnumClass
+{
+    /// <summary>
+    /// Gets or sets the draft document handling setup which controls how draft documents will be deleted after publishing.
+    /// </summary>
+    public enum MyEnumType
+    {
+        [Description("Description 1")]
+        Option1 = 0,
+        [Description("Description 2")]
+        Option2 = 1,
+        [Description("Description 3")]
+        Option3 = 2,
+        [Description("Description 4")]
+        Option4 = 3,
+    }
+
+    public class EnumClass
+    {
+        /// <summary>
+        /// Gets the description for an enum value.
+        /// 
+        /// </summary>
+        public static string GetEnumDescription(Enum value)
+        {
+            // Get the Description attribute value for the enum value
+            System.Reflection.FieldInfo fi = value.GetType().GetField(value.ToString());
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            if (attributes.Length > 0)
+                return attributes[0].Description;
+            else
+                return value.ToString();
+        }
+
+        /// <summary>
+        /// Gets an enum value by the description.
+        /// </summary>
+        public static T GetValueFromDescription<T>(string description)
+        {
+            var type = typeof(T);
+            if (!type.IsEnum) throw new InvalidOperationException();
+            foreach (var field in type.GetFields())
+            {
+                var attribute = Attribute.GetCustomAttribute(field,
+                    typeof(DescriptionAttribute)) as DescriptionAttribute;
+                if (attribute != null)
+                {
+                    if (attribute.Description == description)
+                        return (T)field.GetValue(null);
+                }
+                else
+                {
+                    if (field.Name == description)
+                        return (T)field.GetValue(null);
+                }
+            }
+
+            return default(T);
+        }
+    }
+}
---
+++ Form.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using EnumClass;
+using System.Windows.Forms;
+
+namespace BindingEnumToDropDownList
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        MyEnumType variable;
+
+        public Form()
+        {
+            InitializeComponent();
+
+            Array values = Enum.GetValues(typeof(MyEnumType));
+            foreach (MyEnumType value in values)
+                this.comboBox1.Items.Add(EnumClass.EnumClass.GetEnumDescription(value));
+            this.comboBox1.SelectedItem = EnumClass.EnumClass.GetEnumDescription(variable);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(variable.ToString());
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            variable = EnumClass.EnumClass.GetValueFromDescription<MyEnumType>(this.comboBox1.SelectedItem.ToString());
+        }
+    }
+}
